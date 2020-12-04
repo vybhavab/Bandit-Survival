ae9052ff5423d0a6e8e0561d32b1d83928525fdf
syamhaque <msyamhaque@gmail.com>
2020-12-03 20:08:01 -0800

    added sword assets and scripts

---
+++ Assets/Handle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handle : MonoBehaviour
+{
+    public float damage, speed, knockback;
+    public int durability;
+}
---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public static WeaponManager wm;
+
+    public GameObject blankWeapon;
+    public GameObject[] handles, hilts, blades;
+    public void GenerateWeapon(Vector2 pos)
+    {
+        GameObject weapon = Instantiate(blankWeapon, (Vector3)pos, Quaternion.identity);
+
+        GameObject handle = Instantiate(handles[Random.Range(0, handles.Length)], weapon.transform);
+        handle.transform.localPosition = Vector3.zero;
+        GameObject hilt = Instantiate(hilts[Random.Range(0, hilts.Length)], weapon.transform);
+        hilt.transform.localPosition = Vector3.zero;
+        GameObject blade = Instantiate(blades[Random.Range(0, blades.Length)], weapon.transform);
+        blade.transform.localPosition = Vector3.zero;
+
+        weapon.GetComponent<Weapon>().SetParts(handle, hilt, blade);
+    }
+    
+    void Awake()
+    {
+        wm = this;
+    }
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        for(int i =0; i < 10; i++)
+        {
+            GenerateWeapon(new Vector2(i, 0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
