76752af1e3363cedc3e61df434b825d081af8671
Lucas <lwmoehlenbrock@ucdavis.edu>
2020-12-06 16:48:04 -0600

    Added Food Counter to Player
    Added food counter and level tracker to cave generator scene and fixed issue where player would randomly get stuck when running next to a wall

--- Assets/_Complete-Game/Scripts/Final Project/CameraController.cs
+++ Assets/_Complete-Game/Scripts/Final Project/CameraController.cs
@@ -2,24 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace Completed
 {
-    Bandit player;
+    public class CameraController : MonoBehaviour
+    {
+        Bandit player;
 
-    void Awake()
-    {
-        player =  GameObject.Find("LightBandit").GetComponent<Bandit>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        MapGenerator map = GameObject.Find("MapGenerator").GetComponent<MapGenerator>();
-        transform.position = map.randomStartingLocation;
-    }
+        void Awake()
+        {
+            player = GameObject.Find("LightBandit").GetComponent<Bandit>();
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            MapGenerator map = GameObject.Find("MapGenerator").GetComponent<MapGenerator>();
+            transform.position = map.randomStartingLocation;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, -10);
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        }
     }
 }
