239fc815f7a42428c8a0bb7637c5d4c8a9a40348
Vybhav Achar Bhargav <contact@vybhavb.com>
2020-12-08 17:10:45 -0800

    starting enemy addition

---
+++ Assets/_Complete-Game/Scripts/Final Project/EnemySpawn.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed
+{
+    public class Enemys : MovingObject
+    {
+        // Start is called before the first frame update
+        public int damageToPlayer;
+        public AudioClip attackSound1;
+        public AudioClip attackSound2;
+
+        private Animator animator;
+        private Transform target;
+        private bool skipMove;
+
+        protected override void Start()
+        {
+            CaveGameManager.instance.AddEnemyToList (this);
+            animator = GetComponent<Animator> ();
+            target = GameObject.FindGameObjectWithTag ("Player").transform;
+            base.Start();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        protected override void AttemptMove <T> (int xDir, int yDit)
+        {
+
+        }
+
+        public void MoveEnemy ()
+        {
+
+        }
+
+        protected override void OnCantMove <T> (T component)
+        {
+
+        }
+
+    }
+}
