c6aeee734ec6d8e84c349276caba004f7a04aef9
Deltaus <dsun@ucdavis.edu>
2020-12-13 12:45:10 -0800

    fixed symbol size ratio

---
+++ Assets/_Complete-Game/Scripts/SymbolTile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Completed {
+    public class SymbolTile : MonoBehaviour {
+        // Start is called before the first frame update
+
+        MapGenerator mapGenerator;
+        GameObject[] symbolTiles;
+
+        void Start() {
+            mapGenerator = GameObject.FindWithTag("GameManager").GetComponent<MapGenerator>();
+            symbolTiles = GameObject.FindGameObjectsWithTag("Symbol");
+
+            float size = mapGenerator.baseHeight / 32;
+            foreach (GameObject obj in symbolTiles) {
+                obj.transform.localScale = new Vector3(size, size, 1);
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+            symbolTiles = GameObject.FindGameObjectsWithTag("Symbol");
+
+            float size = mapGenerator.baseHeight / 32;
+            foreach (GameObject obj in symbolTiles) {
+                obj.transform.localScale = new Vector3(size, size, 1);
+            }
+           
+        }
+    }
+}
